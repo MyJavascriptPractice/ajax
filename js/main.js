41dfe72c5e3a2d4e21720117626a288d740e84f5
will <willsmith12289@gmail.com>
2016-08-20 16:12:19 -0400

    work xml ajax request response

--- js/main.js
+++ js/main.js
@@ -18,16 +18,30 @@
 				xhr.responseText;
 
 				var body = document.getElementsByTagName("body")[0];
-				var d = document.createElement("div");
-				body.appendChild(d);
-				var div = document.getElementsByTagName("div")[0];
-				div.innerHTML = xhr.responseText;
+				var heading = xhr.responseXML.getElementsByTagName('heading')[0].firstChild.nodeValue;
+				var h2 = document.createElement("h2");
+				var h2Text = document.createTextNode(heading);
+				h2.appendChild(h2Text);
+				
+
+				var list = document.createElement("ul"),
+						items = xhr.responseXML.getElementsByTagName('items')[0];
+				items = items.getElementsByTagName('item');
+				for (var i = 0; i < items.length; i++) {
+					var item = items[i].firstChild.nodeValue
+							li = document.createElement("li"),
+							liText = document.createTextNode(item);
+					li.appendChild(liText);
+					list.appendChild(li);
+				}
+				body.appendChild(h2);
+				body.appendChild(list);
 				body.removeChild(link);
 			}
 		};
 
 		//open the request get request to open ajax asych
-		xhr.open("Get", "files/ajax.html", true);
+		xhr.open("Get", "files/ajax.xml", true);
 
 		//send request to server
 		xhr.send(null);
