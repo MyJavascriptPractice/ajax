da813d3a527bcd5074f3406e7f172aca2fee6747
will <willsmith12289@gmail.com>
2016-08-20 17:20:25 -0400

    Working GET Requests

---
+++ js/Will.js
@@ -0,0 +1,36 @@
+var Will = {};
+
+Will.createXHR = function(url, options) {
+	var xhr = false;
+
+	if (window.XMLHttpRequest) {
+		xhr = new XMLHttpRequest();
+	}
+	
+	if (xhr) {
+		options = options || {};
+		options.method = options.method || "GET";
+
+		xhr.onreadystatechange = function() {
+			if ((xhr.readyState == 4) && (xhr.status == 200 || xhr.status == 304)) {
+				if (options.complete) {
+					options.complete.call(xhr, JSON.parse(xhr.responseText));
+				}
+			}
+		};
+
+		xhr.open(options.method, url, true);
+			return xhr;
+	} else {
+		return false;
+	}
+
+};
+
+Will.ajax = function(url, options) {
+	var xhr = Will.createXHR(url, options);
+
+	if (xhr) {
+		xhr.send(null);
+	}
+};
--- js/main.js
+++ js/main.js
@@ -3,27 +3,18 @@
 	var link = document.getElementsByTagName("a")[0];
 
 	link.onclick = function() {
-		//xhr object short for xml http request
-		var xhr = new XMLHttpRequest();
-		//handle onreadystatechange request
-		//xhr.readyState() property values
-		// 0 = uninitialized
-		// 1 = Loading
-		// 2 = Loaded
-		// 3 = Interactive(sending response)
-		// 4 = Complete
 
-		xhr.onreadystatechange = function() {
-			if ((xhr.readyState == 4) && (xhr.status == 200 || xhr.status == 304)) {
 
-				var body = document.getElementsByTagName("body")[0];
-						json = JSON.parse(xhr.responseText),
+		Will.ajax('files/ajax.json', {
+			method: "GET",
+			complete: function(response) {
+				var body = document.getElementsByTagName("body")[0],
+						json = response,
 
 						heading = json.heading,
 						h2 = document.createElement("h2"),
 						h2Text = document.createTextNode(heading);
 				h2.appendChild(h2Text);
-				
 
 				var list = document.createElement("ul"),
 						items = json.items;
@@ -38,21 +29,10 @@
 				body.appendChild(h2);
 				body.appendChild(list);
 				body.removeChild(link);
-
 			}
-		};
-
-		//open the request get request to open ajax asych
-		xhr.open("Get", "files/ajax.json", true);
-
-		//send request to server
-		xhr.send(null);
+		});
 
 		return false;
 	};
 
 })();
-
-function processRequest() {
-	
-}
