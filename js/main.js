d8e13f14ef4395cf5ba7dfb54647d2b513a4565f
will <willsmith12289@gmail.com>
2016-08-20 16:31:58 -0400

    working son ajax

--- js/main.js
+++ js/main.js
@@ -15,20 +15,21 @@
 
 		xhr.onreadystatechange = function() {
 			if ((xhr.readyState == 4) && (xhr.status == 200 || xhr.status == 304)) {
-				xhr.responseText;
 
 				var body = document.getElementsByTagName("body")[0];
-				var heading = xhr.responseXML.getElementsByTagName('heading')[0].firstChild.nodeValue;
-				var h2 = document.createElement("h2");
-				var h2Text = document.createTextNode(heading);
+						json = JSON.parse(xhr.responseText),
+
+						heading = json.heading,
+						h2 = document.createElement("h2"),
+						h2Text = document.createTextNode(heading);
 				h2.appendChild(h2Text);
 				
 
 				var list = document.createElement("ul"),
-						items = xhr.responseXML.getElementsByTagName('items')[0];
-				items = items.getElementsByTagName('item');
-				for (var i = 0; i < items.length; i++) {
-					var item = items[i].firstChild.nodeValue
+						items = json.items;
+				
+				for (var key in items) {
+					var item = items[key],
 							li = document.createElement("li"),
 							liText = document.createTextNode(item);
 					li.appendChild(liText);
@@ -37,11 +38,12 @@
 				body.appendChild(h2);
 				body.appendChild(list);
 				body.removeChild(link);
+
 			}
 		};
 
 		//open the request get request to open ajax asych
-		xhr.open("Get", "files/ajax.xml", true);
+		xhr.open("Get", "files/ajax.json", true);
 
 		//send request to server
 		xhr.send(null);
